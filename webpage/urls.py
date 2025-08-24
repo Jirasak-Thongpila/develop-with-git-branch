0574dc8a8862a2f11b64b6b34fd21afcad6a8f70
Jirasak-Thongpire <tle0613828245@gmail.com>
2025-08-24 14:57:56 +0700

    Add random quotes feature with index page and custom quotes support
    - Created URL patterns for index and random quotes views.
    - Implemented views to render index and random quotes templates.
    - Added HTML template for random quotes with styling and JavaScript functionality.
    - Included default quotes and functionality for users to add custom quotes.
    - Enabled clipboard copy and share functionality for quotes.

---
+++ webpage/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("/", views.index, name="index"),
+    path("random-quotes/", views.random_quotes, name="random_quotes"),
+]
--- webpage/views.py
+++ webpage/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def random_quotes(request):
+    return render(request, "random_quotes.html")
+
+def index(request):
+    return render(request, "index.html")
