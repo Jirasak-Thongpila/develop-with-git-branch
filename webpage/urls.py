310b35226c991b6bda5a3713c32c6f4bdcddabf5
Jirasak-Thongpire <tle0613828245@gmail.com>
2025-08-24 15:24:12 +0700

    rename rendom

--- webpage/urls.py
+++ webpage/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("random-quotes/", views.random_quotes, name="random_quotes"),
+    path("random-quotes", views.random_quotes, name="random_quotes"),
 ]
--- webpage/views.py
+++ webpage/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def random_quotes(request):
-    return render(request, "random_quotes.html")
+    return render(request, "random.html")
 
 def index(request):
     return render(request, "index.html")
